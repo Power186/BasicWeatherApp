9fafc1dd1cf7f5c1788a282aa65012c620b9519e
Scott <agentwinburn1@gmail.com>
2020-02-08 20:24:50 +0800

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Power186 on 2/8/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
